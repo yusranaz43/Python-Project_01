b8ce32822f221ad1cb39842bec914e77d93bff42
Yusra Naz <164304204+yusranaz43@users.noreply.github.com>
2025-05-09 07:04:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ if uploaded_files:
             df = pd.read_csv(file)
 
         elif file_ext == ".xlsx":
-            df = pd.read_excel(file, engine="openpyxl")  # Use openpyxl explicitly
-
+            df = pd.read_excel(file)
         else:
             st.error(f"❌ Please upload a valid file format: {file_ext}")
             continue
